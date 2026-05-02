5c608a577ffd1199fa58950ef773d1b64f57ac6c
Haniecki Yoan <haniecki@hotmail.fr>
2026-05-02 22:47:06 +0200

    ajout de la modale avec galerie et suppression des works

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,10 @@
 let body = document.querySelector("body")
 let gallery = document.querySelector(".gallery")
 let h2 = document.querySelector("#portfolio h2")
+let allWorks = []
 const token = sessionStorage.getItem("token")
 const divFilter = document.querySelector(".div-filter")
+
 
 
 fetch("http://localhost:5678/api/categories")
@@ -51,39 +53,105 @@ function afficherWorks(works) {
             const figure = document.createElement("figure")
             const image = document.createElement("img")
             const figcaption = document.createElement("figcaption")
+
             figcaption.innerText = work.title
             image.src = work.imageUrl
             image.alt = work.title
+
             figure.appendChild(image)
             figure.appendChild(figcaption)
             gallery.appendChild(figure)
     });
 }
 
+function afficherWorksModale(works) {
+        const modalGallery = document.querySelector(".modal-gallery")
+        modalGallery.innerHTML= ""
+
+        works.forEach(work => {
+        const figure = document.createElement("figure")
+        const image = document.createElement("img")
+        const trashIcon = document.createElement("i")
+
+        figure.classList.add("modal-figure")
+        image.src = work.imageUrl
+        image.alt = work.title
+        trashIcon.classList.add("fa-solid", "fa-trash-alt")
+        trashIcon.dataset.id = work.id
+
+        trashIcon.addEventListener("click", (e) => {
+            const id = e.target.dataset.id
+
+                fetch(`http://localhost:5678/api/works/${id}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+    }
+})
+    .then (response => {
+        if (response.ok) {
+            console.log("supprimé")
+            figure.remove()
+            allWorks = allWorks.filter(work => work.id !== parseInt(id))
+            gallery.innerHTML = ""
+            afficherWorks(allWorks)
+            
+        } else {
+            console.log("erreur : ", response.status)
+          }
+     })
+})
+        figure.appendChild(image)
+        figure.appendChild(trashIcon)
+        modalGallery.appendChild(figure)
+        })
+}
+
 
     fetch("http://localhost:5678/api/works")
     .then (response => response.json())
     .then (works => {
-        console.log(works[0])
         allWorks = works
         afficherWorks(allWorks)
 })
-
 
 if (token){
     const loginLink = document.querySelector(".login-link")
     loginLink.innerText = "logout"
 
+    const modal = document.querySelector(".modal")
+    const modalWrapper = document.querySelector(".modal-wrapper")
+    const btnClose = document.querySelector(".btn-close")
     const editMod = document.createElement("div")
     const editModName = document.createElement("p")
     const editName = document.createElement("button")
     const titleWrapper = document.createElement("div")
+
     titleWrapper.classList.add("title-wrapper")
     editMod.classList.add("edit-mod")
     editModName.classList.add("edit-mod-name")
     editName.classList.add("edit-name")
     editName.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> modifier'
     editModName.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> Mode édition'
+
+    editName.addEventListener("click", (e) => {
+        modal.style.display = "flex"
+        afficherWorksModale(allWorks)
+    })
+    btnClose.addEventListener("click" , (e) =>{
+        modal.style.display = "none"
+    })
+    modal.addEventListener("click" , (e) => {
+        modal.style.display = "none"
+    })
+    modalWrapper.addEventListener("click" , (e) => {
+        e.stopPropagation()
+    })
+    window.addEventListener("keydown" , (e) => {
+        if (e.key === "Escape" && modal.style.display === "flex") {
+            modal.style.display = "none"
+        }
+    })
     editMod.appendChild(editModName)
     h2.before(titleWrapper)
     titleWrapper.appendChild(h2)
